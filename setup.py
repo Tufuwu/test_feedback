9b4dc461b4b54a0c7d8726f4d9a090a085d40b73
wu <wu@.com>
2025-08-24 18:52:43 +0800

    alephdata/fingerprints/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,25 +1,45 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name='panoptescli',
-    version='1.1.3',
-    url='https://github.com/zooniverse/panoptes-cli',
-    author='Adam McMaster',
-    author_email='adam@zooniverse.org',
-    description=(
-        'A command-line client for Panoptes, the API behind the Zooniverse'
-    ),
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'Click>=6.7,<7.1',
-        'PyYAML>=5.1,<5.5',
-        'panoptes-client>=1.3,<2.0',
-        'humanize>=0.5.1,<1.1',
-        'pathvalidate>=0.29.0,<0.30',
+    name='fingerprints',
+    version='0.6.6',
+    description="A library to generate entity fingerprints.",
+    long_description="",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    entry_points='''
-        [console_scripts]
-        panoptes=panoptes_cli.scripts.panoptes:cli
-    ''',
+    keywords='names people companies normalisation',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/alephdata/fingerprints',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    package_data={
+        '': ['fingerprints/data/types.yml']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='nose.collector',
+    install_requires=[
+        'normality>=0.4.0',
+        'pyyaml',
+        'six'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+        'wheel'
+    ],
+    entry_points={
+    }
 )
