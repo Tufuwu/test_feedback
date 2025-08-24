831146bf6dd175475d7fb19d5c3b227098db9102
wu <wu@.com>
2025-08-24 21:06:46 +0800

    asmeurer/removestar/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,40 +1,30 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="django-statici18n",
-    version="2.1.1",
-    author="Sebastien Fievet",
-    author_email="zyegfryed@gmail.com",
-    url="http://django-statici18n.readthedocs.org/",
-    description="A Django app that provides helper for generating "
-    "Javascript catalog to static files.",
-    long_description=open("README.rst").read(),
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "Django>=2.2",
-        "django-appconf>=1.0",
-    ],
-    license="BSD",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+import versioneer
+
+setuptools.setup(
+    name="removestar",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Aaron Meurer",
+    author_email="asmeurer@gmail.com",
+    description="A tool to automatically replace 'import *' imports with explicit imports in files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.asmeurer.com/removestar/",
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    project_urls={
-        "Source": "https://github.com/zyegfryed/django-statici18n",
-    },
+    entry_points={'console_scripts': [ 'removestar = removestar.__main__:main']},
+    python_requires= '>=3.6',
+    install_requires=[
+        'pyflakes'
+    ],
+    license='MIT',
 )
