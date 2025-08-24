1db1344687409d7ab7c7797da6ee2df3a97a147f
wu <wu@.com>
2025-08-24 19:03:05 +0800

    dlenski/python-vipaccess/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,33 +1,50 @@
+#!/usr/bin/env python3
 from setuptools import setup
+from io import open
+
+with open('description.rst', encoding='utf-8') as f:
+    description = f.read()
 
 setup(
-    name='rnc2rng',
-    version='2.6.1',
-    url='https://github.com/djc/rnc2rng',
-    author='David Mertz',
-    description='RELAX NG Compact to regular syntax conversion library',
-    maintainer='Dirkjan Ochtman',
-    maintainer_email='dirkjan@ochtman.nl',
+    name='python-vipaccess',
+    version='0.13',
+    description="A free software implementation of Symantec's VIP Access application and protocol",
+    long_description=description,
+    url='https://github.com/dlenski/python-vipaccess',
+    author='Daniel Lenski',
+    author_email='dlenski@gmail.com',
+    license='Apache 2.0',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'License :: Public Domain',
-        'Topic :: Text Processing :: Markup :: XML',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    packages=['rnc2rng'],
+    keywords='development',
+    packages=['vipaccess'],
+    install_requires=[
+        # verify consistency with requirements.txt
+        'pycryptodome>=3.6.6',
+        'oath>=1.4.1',
+        'requests',
+    ],
     entry_points={
         'console_scripts': [
-            'rnc2rng = rnc2rng.__main__:main',
+            'vipaccess=vipaccess.__main__:main',
         ],
     },
-    use_2to3=True,
-    install_requires=['rply'],
+    test_requires=[
+        'nose>=1.0',
+    ],
+    test_suite='nose.collector',
 )
