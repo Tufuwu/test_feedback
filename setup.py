fee4435dc3034a1871aeabdd18c4bcab63be5a1c
wu <wu@.com>
2025-08-24 18:28:08 +0800

    beaglecli/beagle/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-import versioneer
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
 setuptools.setup(
-    name="removestar",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Aaron Meurer",
-    author_email="asmeurer@gmail.com",
-    description="A tool to automatically replace 'import *' imports with explicit imports in files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://www.asmeurer.com/removestar/",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    entry_points={'console_scripts': [ 'removestar = removestar.__main__:main']},
-    python_requires= '>=3.6',
-    install_requires=[
-        'pyflakes'
-    ],
-    license='MIT',
-)
+    setup_requires=['pbr'],
+    pbr=True)
