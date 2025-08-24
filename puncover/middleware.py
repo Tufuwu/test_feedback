e9bf8b6a7b31beb10ac5ffc6d9265e3c6c8d6f3c
wu <wu@.com>
2025-08-24 21:45:52 +0800

    in-toto/demo/gpt-4o-3

--- puncover/middleware.py
+++
@@ -1,9 +0,0 @@
-
-class BuilderMiddleware(object):
-    def __init__(self, app, builder):
-        self.app = app
-        self.builder = builder
-
-    def __call__(self, environ, start_response):
-        self.builder.build_if_needed()
-        return self.app(environ, start_response)
