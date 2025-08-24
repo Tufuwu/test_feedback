92259932aaeaafdf4e51fc92c6ce7423ddef80a9
wu <wu@.com>
2025-08-24 18:51:47 +0800

    wimglenn/pytest-structlog/gpt-4o-2

--- docs/gameloop.rst
+++
@@ -1,8 +0,0 @@
-Gameloop Example
-^^^^^^^^^^^^^^^^
-
-Use scheduled events for realtime interative programs:
-
-.. literalinclude:: ../examples/fps.py
-
-Paste it into a file and try it out!
--- setup.py
+++ setup.py
@@ -1,49 +1,23 @@
 from setuptools import setup
-import ast
-import os
-import io
-
-
-def version():
-    """Return version string."""
-    with open(os.path.join("curtsies", "__init__.py")) as input_file:
-        for line in input_file:
-            if line.startswith("__version__"):
-                return ast.parse(line).body[0].value.s
-
-
-def long_description():
-    with open("README.md", encoding="utf-8") as f:
-        return f.read()
-
 
 setup(
-    name="curtsies",
-    version=version(),
-    description="Curses-like terminal wrapper, with colored strings!",
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/bpython/curtsies",
-    author="Thomas Ballinger",
-    author_email="thomasballinger@gmail.com",
+    name="pytest-structlog",
+    version="0.5",
+    url="https://github.com/wimglenn/pytest-structlog",
+    description="Structured logging assertions",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    author="Wim Glenn",
+    author_email="hey@wimglenn.com",
     license="MIT",
-    packages=["curtsies"],
-    install_requires=[
-        "blessings>=1.5",
-        "wcwidth>=0.1.4",
-    ],
-    tests_require=[
-        "pyte",
-        "pytest",
-    ],
+    install_requires=["pytest", "structlog"],
+    py_modules=["pytest_structlog"],
+    entry_points={"pytest11": ["pytest-structlog=pytest_structlog"]},
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
+        "Framework :: Pytest",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    zip_safe=False,
+    options={"bdist_wheel": {"universal": "1"}},
 )
