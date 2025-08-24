488fa6805110174b441cb2f94ff4bd885e7155f3
wu <wu@.com>
2025-08-24 23:47:45 +0800

    ncclient/ncclient/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,58 +1,76 @@
-"""
-Django-MongoEngine
-------------------
+# Copyright 2009 Shikhar Bhushan
+# Copyright 201[2-5] Leonidas Poulopoulos (@leopoul)
+# Copyright 2013 Ebben Aries
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Django support for MongoDB using MongoEngine.
-
-This is work-in-progress. Some things working, some don't. Fix what you need and make
-pull-request.
-
-Links
-`````
-
-* `development version
-  <https://github.com/MongoEngine/django-mongoengine>`_
-
-"""
 from setuptools import setup, find_packages
+from distutils.command.install import install as _install
+
 import sys
-import os
+import platform
+import codecs
+import versioneer
+
+__author__ = "Shikhar Bhushan, Leonidas Poulopoulos, Ebben Aries, Einar Nilsen-Nygaard"
+__author_email__ = "shikhar@schmizz.net, lpoulopoulos@verisign.com, exa@dscp.org, einarnn@gmail.com"
+__licence__ = "Apache 2.0"
+
+if sys.version_info.major == 2 and sys.version_info.minor < 7:
+    print ("Sorry, Python < 2.7 is not supported")
+    exit()
+
+#parse requirements
+req_lines = [line.strip() for line in open("requirements.txt").readlines()]
+install_reqs = list(filter(None, req_lines))
+
+test_req_lines = [line.strip() for line in open("test-requirements.txt").readlines()]
+test_reqs = list(filter(None, test_req_lines))
+
+with codecs.open('README.rst', 'r', encoding='utf8') as file:
+    long_description = file.read()
 
 
-__version__ = '0.4.6'
-__description__ = 'Django support for MongoDB via MongoEngine'
-__license__ = 'BSD'
-__author__ = 'Ross Lawley'
-__email__ = 'ross.lawley@gmail.com'
+setup(name='ncclient',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description="Python library for NETCONF clients",
+      long_description = long_description,
+      author=__author__,
+      author_email=__author_email__,
+      url="https://github.com/ncclient/ncclient",
+      packages=find_packages(exclude=['test', 'test.*']),
+      install_requires=install_reqs,
+      tests_require=test_reqs,
+      license=__licence__,
+      platforms=["Posix; OS X; Windows"],
+      keywords=['NETCONF', 'NETCONF Python client', 'Juniper Optimization', 'Cisco NXOS Optimization'],
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: System :: Networking',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ])
 
 
-sys.path.insert(0, os.path.dirname(__file__))
 
 
-setup(
-    name='django-mongoengine',
-    version=__version__,
-    url='https://github.com/mongoengine/django-mongoengine',
-    download_url='https://github.com/mongoengine/django-mongoengine/tarball/master',
-    license=__license__,
-    author=__author__,
-    author_email=__email__,
-    description=__description__,
-    long_description=__doc__,
-    zip_safe=False,
-    platforms='any',
-    install_requires=["django>2.2,<3.3", "mongoengine>=0.14"],
-    packages=find_packages(exclude=('doc', 'docs',)),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Framework :: Django'
-    ]
-)
+
+
+
