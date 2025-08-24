3f5be0fadb70d74c4fa24659c4000985252fa643
wu <wu@.com>
2025-08-24 19:35:21 +0800

    delgan/loguru/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,78 +1,72 @@
-#!/usr/bin/env python
+import re
 
-# Prepare a release:
-#
-#  - git pull --rebase  # check that there is no incoming changesets
-#  - check version in ptrace/version.py and doc/conf.py
-#  - set release date in doc/changelog.rst
-#  - check that "python3 setup.py sdist" contains all files tracked by
-#    the SCM (Git): update MANIFEST.in if needed
-#  - git commit -a -m "prepare release VERSION"
-#  - Remove untracked files/dirs: git clean -fdx
-#  - run tests, type: tox --parallel auto
-#  - git push
-#  - check GitHub Actions status:
-#    https://github.com/vstinner/python-ptrace/actions
-#
-# Release a new version:
-#
-#  - git tag VERSION
-#  - Remove untracked files/dirs: git clean -fdx
-#  - python3 setup.py sdist bdist_wheel
-#  - git push --tags
-#  - twine upload dist/*
-#
-# After the release:
-#
-#  - increment version in  ptrace/version.py and doc/conf.py
-#  - git commit -a -m "post-release"
-#  - git push
-
-from imp import load_source
-from os import path
 try:
-    # setuptools supports bdist_wheel
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+with open("loguru/__init__.py", "r") as file:
+    regex_version = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+    version = re.search(regex_version, file.read(), re.MULTILINE).group(1)
 
-MODULES = ["ptrace", "ptrace.binding", "ptrace.syscall", "ptrace.syscall.linux", "ptrace.debugger"]
+with open("README.rst", "rb") as file:
+    readme = file.read().decode("utf-8")
 
-SCRIPTS = ("strace.py", "gdb.py")
-
-CLASSIFIERS = [
-    'Intended Audience :: Developers',
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'License :: OSI Approved :: GNU General Public License (GPL)',
-    'Operating System :: OS Independent',
-    'Natural Language :: English',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-]
-
-with open('README.rst') as fp:
-    LONG_DESCRIPTION = fp.read()
-
-ptrace = load_source("version", path.join("ptrace", "version.py"))
-PACKAGES = {}
-for name in MODULES:
-    PACKAGES[name] = name.replace(".", "/")
-
-install_options = {
-    "name": ptrace.PACKAGE,
-    "version": ptrace.__version__,
-    "url": ptrace.WEBSITE,
-    "download_url": ptrace.WEBSITE,
-    "author": "Victor Stinner",
-    "description": "python binding of ptrace",
-    "long_description": LONG_DESCRIPTION,
-    "classifiers": CLASSIFIERS,
-    "license": ptrace.LICENSE,
-    "packages": list(PACKAGES.keys()),
-    "package_dir": PACKAGES,
-    "scripts": SCRIPTS,
-}
-
-setup(**install_options)
+setup(
+    name="loguru",
+    version=version,
+    packages=["loguru"],
+    package_data={"loguru": ["__init__.pyi", "py.typed"]},
+    description="Python logging made (stupidly) simple",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    author="Delgan",
+    author_email="delgan.py@gmail.com",
+    url="https://github.com/Delgan/loguru",
+    download_url="https://github.com/Delgan/loguru/archive/{}.tar.gz".format(version),
+    project_urls={
+        "Changelog": "https://github.com/Delgan/loguru/blob/master/CHANGELOG.rst",
+        "Documentation": "https://loguru.readthedocs.io/en/stable/index.html",
+    },
+    keywords=["loguru", "logging", "logger", "log"],
+    license="MIT license",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: System :: Logging",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    install_requires=[
+        "colorama>=0.3.4 ; sys_platform=='win32'",
+        "aiocontextvars>=0.2.0 ; python_version<'3.7'",
+        "win32-setctime>=1.0.0 ; sys_platform=='win32'",
+    ],
+    extras_require={
+        "dev": [
+            "black>=19.10b0 ; python_version>='3.6'",
+            "colorama>=0.3.4",
+            "docutils==0.16",
+            "flake8>=3.7.7",
+            "isort>=5.1.1 ; python_version>='3.6'",
+            "tox>=3.9.0",
+            "pytest>=4.6.2",
+            "pytest-cov>=2.7.1",
+            "Sphinx>=4.1.1 ; python_version>='3.6'",
+            "sphinx-autobuild>=0.7.1 ; python_version>='3.6'",
+            "sphinx-rtd-theme>=0.4.3 ; python_version>='3.6'",
+        ]
+    },
+    python_requires=">=3.5",
+)
