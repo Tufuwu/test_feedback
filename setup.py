f878ad083465cfa83a6676d5886b683a67d31770
wu <wu@.com>
2025-08-24 23:35:01 +0800

    dabapps/django-zen-queries/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,52 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup
+import re
 import os
+import sys
 
-from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
+name = "django-zen-queries"
+package = "zen_queries"
+description = "Explicit control over query execution in Django applications."
+url = "https://github.com/dabapps/django-zen-queries"
+author = "DabApps"
+author_email = "hello@dabapps.com"
+license = "BSD"
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+long_description = description + "\n\nFor full details, see https://github.com/dabapps/django-zen-queries"
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, "__init__.py")).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(
+        1
+    )
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [
+        (dirpath.replace(package + os.sep, "", 1), filenames)
+        for dirpath, dirnames, filenames in os.walk(package)
+        if not os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename) for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    args = {"version": get_version(package)}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
+    sys.exit()
+
 
 setup(
-    name="django-phone-verify",
-    version="2.0.1",
-    packages=find_packages(),
-    include_package_data=True,
-    license="GPLv3",
-    description="A Django app to support phone number verification using security code sent via SMS.",
-    long_description=README,
-    url="https://github.com/CuriousLearner/django-phone-verify",
-    author="Sanyam Khurana",
-    author_email="sanyam@sanyamkhurana.com",
-    install_requires=[
-        "django>=2.1.5",
-        "djangorestframework>=3.9.0",
-        "PyJWT>=1.7.1",
-        "python-dotenv>=0.10.0",
-        "phonenumbers>=8.10.2",
-        "django-phonenumber-field>=2.1.0",
-        "twilio>=6.21.0",
-        "nexmo>=2.4.0",
-    ],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    long_description=long_description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
 )
