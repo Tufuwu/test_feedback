b00f4db90229468c112e812bcd7d273153bcfec9
wu <wu@.com>
2025-08-25 00:00:27 +0800

    vstinner/python-ptrace/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,51 +1,78 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import os
-import sys
+# Prepare a release:
+#
+#  - git pull --rebase  # check that there is no incoming changesets
+#  - check version in ptrace/version.py and doc/conf.py
+#  - set release date in doc/changelog.rst
+#  - check that "python3 setup.py sdist" contains all files tracked by
+#    the SCM (Git): update MANIFEST.in if needed
+#  - git commit -a -m "prepare release VERSION"
+#  - Remove untracked files/dirs: git clean -fdx
+#  - run tests, type: tox --parallel auto
+#  - git push
+#  - check GitHub Actions status:
+#    https://github.com/vstinner/python-ptrace/actions
+#
+# Release a new version:
+#
+#  - git tag VERSION
+#  - Remove untracked files/dirs: git clean -fdx
+#  - python3 setup.py sdist bdist_wheel
+#  - git push --tags
+#  - twine upload dist/*
+#
+# After the release:
+#
+#  - increment version in  ptrace/version.py and doc/conf.py
+#  - git commit -a -m "post-release"
+#  - git push
 
-
+from imp import load_source
+from os import path
 try:
+    # setuptools supports bdist_wheel
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
 
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
-import djangorestframework_camel_case
+MODULES = ["ptrace", "ptrace.binding", "ptrace.syscall", "ptrace.syscall.linux", "ptrace.debugger"]
 
-setup(
-    name="djangorestframework-camel-case",
-    version=djangorestframework_camel_case.__version__,
-    description="Camel case JSON support for Django REST framework.",
-    long_description=readme + "\n\n" + history,
-    long_description_content_type="text/x-rst",
-    author="Vitaly Babiy",
-    author_email="vbabiy86@gmail.com",
-    url="https://github.com/vbabiy/djangorestframework-camel-case",
-    packages=["djangorestframework_camel_case"],
-    package_dir={"djangorestframework_camel_case": "djangorestframework_camel_case"},
-    include_package_data=True,
-    python_requires=">=3.5",
-    install_requires=[],
-    license="BSD",
-    zip_safe=False,
-    keywords="djangorestframework_camel_case",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    test_suite="tests",
-)
+SCRIPTS = ("strace.py", "gdb.py")
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+]
+
+with open('README.rst') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+ptrace = load_source("version", path.join("ptrace", "version.py"))
+PACKAGES = {}
+for name in MODULES:
+    PACKAGES[name] = name.replace(".", "/")
+
+install_options = {
+    "name": ptrace.PACKAGE,
+    "version": ptrace.__version__,
+    "url": ptrace.WEBSITE,
+    "download_url": ptrace.WEBSITE,
+    "author": "Victor Stinner",
+    "description": "python binding of ptrace",
+    "long_description": LONG_DESCRIPTION,
+    "classifiers": CLASSIFIERS,
+    "license": ptrace.LICENSE,
+    "packages": list(PACKAGES.keys()),
+    "package_dir": PACKAGES,
+    "scripts": SCRIPTS,
+}
+
+setup(**install_options)
