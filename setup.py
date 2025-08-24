c800ad5f9b06b5b63a95fbcbdc5179e3ac5f018b
wu <wu@.com>
2025-08-24 23:58:22 +0800

    theolind/pymysensors/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,68 +1,53 @@
-# To increment version
-# Check you have ~/.pypirc filled in
-# git tag x.y.z
-# git push && git push --tags
-# rm -rf dist; python setup.py sdist bdist_wheel
-# TEST: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# twine upload dist/*
-
-from codecs import open
-import re
+"""Setup file for mysensors package."""
+from pathlib import Path
 
 from setuptools import setup, find_packages
-import sys
 
-author = "Danny Price, Ellert van der Velden and contributors"
+PROJECT_DIR = Path(__file__).parent.resolve()
+VERSION = (PROJECT_DIR / "mysensors" / "VERSION").read_text().strip()
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+README_FILE = PROJECT_DIR / "README.md"
+LONG_DESCR = README_FILE.read_text(encoding="utf-8")
 
-with open("requirements.txt", 'r') as fh:
-    requirements = fh.read().splitlines()
+REQUIRES = [
+    "click",
+    "crcmod>=1.7",
+    "getmac",
+    "IntelHex>=2.2.1",
+    "pyserial>=3.4",
+    "pyserial-asyncio>=0.4",
+    "voluptuous>=0.11.1",
+]
+EXTRAS = {"mqtt-client": ["paho-mqtt"]}
 
-with open("requirements_test.txt", 'r') as fh:
-    test_requirements = fh.read().splitlines()
 
-# Read the __version__.py file
-with open('hickle/__version__.py', 'r') as f:
-    vf = f.read()
-
-# Obtain version from read-in __version__.py file
-version = re.search(r"^_*version_* = ['\"]([^'\"]*)['\"]", vf, re.M).group(1)
-
-setup(name='hickle',
-      version=version,
-      description='Hickle - an HDF5 based version of pickle',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author=author,
-      author_email='dan@thetelegraphic.com',
-      url='http://github.com/telegraphic/hickle',
-      download_url=('https://github.com/telegraphic/hickle/archive/v%s.zip'
-                    % (version)),
-      platforms='Cross platform (Linux, Mac OSX, Windows)',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved',
-          'Natural Language :: English',
-          'Operating System :: MacOS',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: Unix',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Utilities',
-          ],
-      keywords=['pickle', 'hdf5', 'data storage', 'data export'],
-      install_requires=requirements,
-      tests_require=test_requirements,
-      python_requires='>=3.5',
-      packages=find_packages(),
-      zip_safe=False,
+setup(
+    name="pymysensors",
+    version=VERSION,
+    description="Python API for talking to a MySensors gateway",
+    long_description=LONG_DESCR,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theolind/pymysensors",
+    author="Theodor Lindquist",
+    author_email="theodor.lindquist@gmail.com",
+    license="MIT License",
+    install_requires=REQUIRES,
+    extras_require=EXTRAS,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    python_requires=">=3.5.3",
+    entry_points={"console_scripts": ["pymysensors = mysensors.cli:cli"]},
+    keywords=["sensor", "actuator", "IoT", "DYI"],
+    zip_safe=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Home Automation",
+    ],
 )
