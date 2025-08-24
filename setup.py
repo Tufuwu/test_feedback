3d7f341758896ff410ab826a754249a35f794ca7
wu <wu@.com>
2025-08-24 21:29:11 +0800

    xflr6/graphviz/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,49 +1,43 @@
-from setuptools import setup
-import ast
-import os
-import io
-
-
-def version():
-    """Return version string."""
-    with open(os.path.join("curtsies", "__init__.py")) as input_file:
-        for line in input_file:
-            if line.startswith("__version__"):
-                return ast.parse(line).body[0].value.s
-
-
-def long_description():
-    with open("README.md", encoding="utf-8") as f:
-        return f.read()
-
+import pathlib
+from setuptools import setup, find_packages
 
 setup(
-    name="curtsies",
-    version=version(),
-    description="Curses-like terminal wrapper, with colored strings!",
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/bpython/curtsies",
-    author="Thomas Ballinger",
-    author_email="thomasballinger@gmail.com",
-    license="MIT",
-    packages=["curtsies"],
-    install_requires=[
-        "blessings>=1.5",
-        "wcwidth>=0.1.4",
-    ],
-    tests_require=[
-        "pyte",
-        "pytest",
-    ],
+    name='graphviz',
+    version='0.17.1.dev0',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Simple Python interface for Graphviz',
+    keywords='graph visualization dot render',
+    license='MIT',
+    url='https://github.com/xflr6/graphviz',
+    project_urls={
+        'Documentation': 'https://graphviz.readthedocs.io',
+        'Changelog': 'https://graphviz.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/xflr6/graphviz/issues',
+        'CI': 'https://github.com/xflr6/graphviz/actions',
+        'Coverage': 'https://codecov.io/gh/xflr6/graphviz',
+    },
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.6',
+    extras_require={
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['mock>=3', 'pytest>=5.2', 'pytest-mock>=2', 'pytest-cov'],
+        'docs': ['sphinx>=1.8', 'sphinx-autodoc-typehints', 'sphinx-rtd-theme'],
+    },
+    long_description=pathlib.Path('README.rst').read_text(encoding='utf-8'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Visualization',
     ],
-    zip_safe=False,
 )
