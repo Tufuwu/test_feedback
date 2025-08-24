47fa4b47aa9fac6647601458d990b733f1cdaaea
wu <wu@.com>
2025-08-24 21:57:57 +0800

    pycqa/pyflakes/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,39 +1,67 @@
-from setuptools import setup, find_packages, __version__
-from os import path
+#!/usr/bin/env python
+# Copyright 2005-2011 Divmod, Inc.
+# Copyright 2013 Florent Xicluna.  See LICENSE file for details
+from __future__ import with_statement
 
-desc_file = "README.md"
+import os.path
 
-with open(desc_file, "r") as fh:
-    long_description = fh.read()
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    extra = {'scripts': ["bin/pyflakes"]}
+else:
+    extra = {
+        'test_suite': 'pyflakes.test',
+        'entry_points': {
+            'console_scripts': ['pyflakes = pyflakes.api:main'],
+        },
+    }
 
-here = path.abspath(path.dirname(__file__))
-# get the dependencies and installs
-with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+def get_version(fname=os.path.join('pyflakes', '__init__.py')):
+    with open(fname) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
+
+
+def get_long_description():
+    descr = []
+    for fname in ('README.rst',):
+        with open(fname) as f:
+            descr.append(f.read())
+    return '\n\n'.join(descr)
+
 
 setup(
-    name="casbin_sqlalchemy_adapter",
-    author="TechLee",
-    author_email="techlee@qq.com",
-    description="SQLAlchemy Adapter for PyCasbin",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pycasbin/sqlalchemy-adapter",
-    keywords=["casbin", "SQLAlchemy", "casbin-adapter", "rbac", "access control", "abac", "acl", "permission"],
-    packages=find_packages(),
-    install_requires=install_requires,
-    python_requires=">=3.3",
-    license="Apache 2.0",
+    name="pyflakes",
+    license="MIT",
+    version=get_version(),
+    description="passive checker of Python programs",
+    long_description=get_long_description(),
+    author="A lot of people",
+    author_email="code-quality@python.org",
+    url="https://github.com/PyCQA/pyflakes",
+    packages=["pyflakes", "pyflakes.scripts", "pyflakes.test"],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
     ],
-    data_files=[desc_file],
-)
+    **extra)
