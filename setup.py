58e439761a4c8cee531e8ff3239b87546bda4389
wu <wu@.com>
2025-08-24 19:20:57 +0800

    polyconseil/django-cid/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,23 +1,49 @@
-#!/usr/bin/env python
-"""Setup to install Bitcoin.de API Python Module."""
-from distutils.core import setup
-setup(name='btcde',
-      version='4.0',
-      py_modules=['btcde'],
-      install_requires=['requests', 'future'],
-      description='API Wrapper for Bitcoin.de Trading API.',
-      url='https://github.com/peshay/btcde',
-      author='Andreas Hubert',
-      author_email='anhubert@gmail.com',
-      license='MIT',
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Developers',
-                   'Topic :: Software Development :: Libraries :: Application '
-                   'Frameworks',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9', ],
-      keywords='bitcoin.de bitcoin btc api',
+from setuptools import setup, find_packages
 
-      )
+
+def clean_history(history):
+    # PyPI does not allow the `raw` directive. We'll laboriously
+    # replace it. Hang tight, it's going to be ugly.
+    history = history.replace('|backward-incompatible|', '**backward incompatible:** ')
+    lines = []
+    for line in history.split('\n'):
+        if line.startswith('.. role:: raw-html'):
+            break
+        lines.append(line)
+    return '\n'.join(lines)
+
+
+readme = open('README.rst').read()
+changelog = clean_history(open('HISTORY.rst').read())
+
+setup(
+    name='django-cid',
+    version='2.2.dev0',
+    description="""Correlation IDs in Django for debugging requests""",
+    long_description=readme + '\n\n' + changelog,
+    author='Snowball One',
+    author_email='opensource+django-cid@polyconseil.fr',
+    maintainer="Polyconseil",
+    maintainer_email="opensource+django-cid@polyconseil.fr",
+    url='https://github.com/Polyconseil/django-cid',
+    packages=find_packages(exclude=('sandbox*', 'tests*')),
+    include_package_data=True,
+    install_requires=[
+        'django>=2.2',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django logging correlation id debugging',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
