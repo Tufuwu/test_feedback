911076cd0adc9604f3e7aa395edc6ba076f3f245
wu <wu@.com>
2025-08-24 21:39:09 +0800

    django-import-export/django-import-export/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,56 +1,57 @@
-#!/usr/bin/env python
-from pathlib import Path
+import os
+
 from setuptools import find_packages, setup
 
-
-REQUIREMENTS = [
-    'django>=2.2',
-    'django-classy-tags>=1',
-]
-
+VERSION = __import__("import_export").__version__
 
 CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
     'Framework :: Django',
     'Framework :: Django :: 2.2',
     'Framework :: Django :: 3.1',
     'Framework :: Django :: 3.2',
-    'Topic :: Internet :: WWW/HTTP',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
     'Topic :: Software Development',
-    'Topic :: Software Development :: Libraries',
 ]
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+install_requires = [
+    'diff-match-patch',
+    'Django>=2.2',
+    'tablib[html,ods,xls,xlsx,yaml]>=3.0.0',
+]
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
 
 setup(
-    name='django-sekizai',
-    version='2.0.0',
-    author='Jonas Obrist',
-    author_email='ojiidotch@gmail.com',
-    maintainer='Django CMS Association and contributors',
-    maintainer_email='info@django-cms.org',
-    url='https://github.com/django-cms/django-sekizai',
-    license='BSD-3-Clause',
-    description='Django Sekizai',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    packages=find_packages(),
-    python_requires='>=3.7',
+    name="django-import-export",
+    description="Django application and library for importing and exporting"
+                " data with included admin integration.",
+    long_description=readme,
+    version=VERSION,
+    author="Informatika Mihelac",
+    author_email="bmihelac@mihelac.org",
+    license='BSD License',
+    platforms=['OS Independent'],
+    url="https://github.com/django-import-export/django-import-export",
+    project_urls={
+        "Documentation": "https://django-import-export.readthedocs.io/en/stable/",
+        "Changelog": "https://django-import-export.readthedocs.io/en/stable/changelog.html",
+    },
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    zip_safe=False,
-    install_requires=REQUIREMENTS,
+    install_requires=install_requires,
+    python_requires=">=3.6",
     classifiers=CLASSIFIERS,
-    test_suite='tests.settings.run',
+    zip_safe=False,
 )
