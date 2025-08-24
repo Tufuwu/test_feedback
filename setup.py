704720a7ac3a4283b80229df587035ba13a51c79
wu <wu@.com>
2025-08-24 23:20:56 +0800

    mgaitan/sphinxcontrib-mermaid/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,42 +1,64 @@
-import os
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+import io
+from setuptools import setup, find_packages
 
-this_directory = os.path.dirname(__file__)
-module_path = os.path.join(this_directory, 'flask_bcrypt.py')
-version_line = [line for line in open(module_path)
-                if line.startswith('__version_info__')][0]
-with open(os.path.join(this_directory, 'README.markdown')) as f:
-    long_description = f.read()
+readme = io.open('README.rst', encoding="utf-8").read()
+changes = io.open('CHANGELOG.rst', encoding="utf-8").read()
+version = '0.7.1'
 
-__version__ = '.'.join(eval(version_line.split('__version_info__ = ')[-1]))
+
+def long_description():
+    """
+    return readme + changes, removing directive blocks that are only valid in the context
+    of sphinx doc"""
+
+    def remove_block(text, token, margin=0):
+        input_lines = text.splitlines()
+        for i, l in enumerate(input_lines):
+            if l.startswith(token):
+                break
+        start = i
+        end = input_lines.index("", start + margin)
+        return "\n".join(input_lines[:start] + input_lines[end:])
+
+    readme_ = remove_block(readme, ".. mermaid::", margin=2)
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. versionchanged::")
+    return "{}\n\n{}".format(readme_, changes)
+
 
 setup(
-    name='Flask-Bcrypt',
-    version=__version__,
-    url='https://github.com/maxcountryman/flask-bcrypt',
+    name='sphinxcontrib-mermaid',
+    version=version,
+    url='https://github.com/mgaitan/sphinxcontrib-mermaid',
+    download_url='https://pypi.python.org/pypi/sphinxcontrib-mermaid',
     license='BSD',
-    author='Max Countryman',
-    author_email='maxc@me.com',
-    description='Brcrypt hashing for Flask.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    py_modules=['flask_bcrypt'],
-    zip_safe=False,
-    platforms='any',
-    install_requires=['Flask', 'bcrypt>=3.1.1'],
+    author=u'Martín Gaitán',
+    author_email='gaitan@gmail.com',
+    description='Mermaid diagrams in yours Sphinx powered docs',
+    long_description=long_description(),
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
     ],
-    test_suite='test_bcrypt'
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['sphinxcontrib'],
 )
---
+++ tests/roots/test-markdown/index.md
@@ -0,0 +1,9 @@
+# Hi from Markdown!
+
+```{mermaid}
+
+    sequenceDiagram
+      participant Alice
+      participant Bob
+      Alice->John: Hello John, how are you?
+```
