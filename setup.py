9e16ba3e3473820824f7b82d74c3e3cc120b0b1f
wu <wu@.com>
2025-08-24 19:12:11 +0800

    keeprocking/pygelf/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,53 +1,28 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2016-2019 Frederic Guillot
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 from setuptools import setup
 
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-
 setup(
-    name='kanboard',
-    version='1.1.1',
-    description='Client library for Kanboard API',
-    long_description=readme(),
-    keywords='kanboard api client',
-    url='https://github.com/kanboard/python-api-client',
-    author='Frederic Guillot',
-    author_email='fred@kanboard.net',
+    name='pygelf',
+    version='0.4.2',
+    packages=['pygelf'],
+    description='Logging handlers with GELF support',
+    keywords='logging udp tcp ssl tls graylog2 graylog gelf',
+    author='Ivan Mukhin',
+    author_email='muhin.ivan@gmail.com',
+    url='https://github.com/keeprocking/pygelf',
+    long_description=open('README.rst').read(),
     license='MIT',
-    py_modules=['kanboard'],
-    test_suite='test_kanboard',
     classifiers=[
         'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ]
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: System :: Logging',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
