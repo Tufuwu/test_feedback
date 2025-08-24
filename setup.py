fb22ea0c3000e54f5942da404376ccfb10ca1fec
wu <wu@.com>
2025-08-24 18:31:31 +0800

    django-fluent/django-fluent-blogs/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,83 +1,90 @@
-"""
-Installation script for olefile using setuptools
+#!/usr/bin/env python
+import codecs
+import os
+import re
+import sys
+from os import path
 
-To install this package, run:
-    python setup.py install
+from setuptools import find_packages, setup
 
-"""
+# When creating the sdist, make sure the django.mo file also exists:
+if "sdist" in sys.argv or "develop" in sys.argv:
+    os.chdir("fluent_blogs")
+    try:
+        from django.core import management
 
-
-# --- IMPORTS -----------------------------------------------------------------
-
-from setuptools import setup
-
-from olefile import __version__, __author__
-
-
-# --- METADATA ----------------------------------------------------------------
-
-name         = "olefile"
-version      = __version__
-desc         = "Python package to parse, read and write Microsoft OLE2 files (Structured Storage or Compound Document, Microsoft Office)"
-author       = __author__
-author_email = "nospam@decalage.info"
-url          = "https://www.decalage.info/python/olefileio"
-license      = "BSD"
-download_url = "https://github.com/decalage2/olefile/tarball/master"
-
-with open('README.md') as f:
-    long_desc = f.read()
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Intended Audience :: Information Technology",
-    "Intended Audience :: Science/Research",
-    "Intended Audience :: System Administrators",
-    "License :: OSI Approved :: BSD License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: Implementation :: CPython",
-    "Programming Language :: Python :: Implementation :: PyPy",
-    "Topic :: Software Development :: Libraries :: Python Modules"
-]
-
-# --- PACKAGES ----------------------------------------------------------------
-
-packages = [
-    "olefile",
-]
+        management.call_command("compilemessages", stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if "sdist" in sys.argv:
+            raise
+    finally:
+        os.chdir("..")
 
 
-# === MAIN ====================================================================
-
-def main():
-    setup(
-        name=name,
-        version=version,
-        description=desc,
-        long_description=long_desc,
-        long_description_content_type='text/markdown',
-        classifiers=classifiers,
-        author=author,
-        author_email=author_email,
-        url=url,
-        license=license,
-#        package_dir=package_dir,
-        packages=packages,
-        download_url=download_url,
-#        data_files=data_files,
-#        scripts=scripts,
-        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
-    )
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
-if __name__ == "__main__":
-    main()
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="django-fluent-blogs",
+    version=find_version("fluent_blogs", "__init__.py"),
+    license="Apache 2.0",
+    install_requires=[
+        "django-fluent-contents>=2.0.7",
+        "django-fluent-utils>=2.0.1",
+        "django-categories-i18n>=1.1.1",
+        "django-parler>=2.0.1",
+        "django-slug-preview>=1.0.4",
+        "django-tag-parser>=3.2",
+    ],
+    requires=[
+        "Django (>=2.2)",
+    ],
+    extras_require={
+        "tests": [
+            "django-fluent-pages>=2.0.7",
+            "django-wysiwyg>=0.7.1",
+        ],
+        "blogpage": ["django-fluent-pages>=2.0.7"],
+        "taggit": ["taggit", "taggit-autosuggest"],
+    },
+    description="A blog engine with flexible block contents (based on django-fluent-contents).",
+    long_description=read("README.rst"),
+    author="Diederik van der Boor",
+    author_email="opensource@edoburu.nl",
+    url="https://github.com/edoburu/django-fluent-blogs",
+    download_url="https://github.com/edoburu/django-fluent-blogs/zipball/master",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite="runtests",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
