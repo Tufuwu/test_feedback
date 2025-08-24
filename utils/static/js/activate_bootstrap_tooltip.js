b1ecdb2bf81504e9661809ad2a61a863cbcb5384
wu <wu@.com>
2025-08-24 21:43:37 +0800

    etesync/etesync-dav/gpt-4o-3

--- utils/static/js/activate_bootstrap_tooltip.js
+++
@@ -1,6 +0,0 @@
-// Boostrap tooltip are not activated by default to save running time
-// Add this script to the page if it uses tooltips
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
--- utils/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-def raisesAssertionError(method, *args, **kwargs):
-    try:
-        method(*args, **kwargs)
-    except AssertionError as error:
-        return error
-    else:
-        raise AssertionError(f"Assertionerror not raised on {method_name}")
