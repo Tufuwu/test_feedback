2e3a9a86d2b0e371dcb5fdb6ee6495498031b58b
wu <wu@.com>
2025-08-24 19:29:26 +0800

    elmotec/circlify/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,47 +1,45 @@
-# Setup for epydemic
-#
-# Copyright (C) 2017--2021 Simon Dobson
-#
-# This file is part of epydemic, epidemic network simulations in Python.
-#
-# epydemic is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# epydemic is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Packaging script."""
+
+import os
 
 from setuptools import setup
 
-with open('README.rst') as f:
-    longDescription = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, "README.rst")).read()
 
-setup(name = 'epydemic',
-      version = '1.4.2',
-      description = 'Epidemic network simulations in Python',
-      long_description = longDescription,
-      url = 'http://github.com/simoninireland/epydemic',
-      author = 'Simon Dobson',
-      author_email = 'simoninireland@gmail.com',
-      license = 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-      classifiers = [ 'Development Status :: 4 - Beta',
-                      'Intended Audience :: Science/Research',
-                      'Intended Audience :: Developers',
-                      'Programming Language :: Python :: 3.6',
-                      'Programming Language :: Python :: 3.7',
-                      'Programming Language :: Python :: 3.8',
-                      'Programming Language :: Python :: 3.9',
-                      'Topic :: Scientific/Engineering' ],
-      python_requires = '>=3.6',
-      packages = [ 'epydemic' ],
-      package_data = { 'epydemic': [ 'py.typed' ] },
-      zip_safe = False,
-      install_requires = [ "networkx >= 2.4", "epyc >= 1.2.1", "pandas", "numpy >= 1.18", "mpmath",  ],
-      extra_requires = { ':python_version < 3.8': [ 'typing_extensions' ] },
+setup(
+    name="circlify",
+    description="Circle packing algorithm for Python",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    version="0.14.0",
+    author="Elmotec",
+    author_email="elmotec@gmx.com",
+    license="MIT",
+    keywords="circle packing enclosure hierarchy graph display visualization",
+    url="http://github.com/elmotec/circlify",
+    py_modules=["circlify"],
+    test_suite="tests",
+    setup_requires=[],
+    tests_require=[],
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Developers",
+    ],
 )
