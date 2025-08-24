d6e86dc78511ccdf894629522d7c7030dd1643d2
wu <wu@.com>
2025-08-24 21:15:07 +0800

    jasonacox/tinytuya/gpt-4o-3

--- examples/bad/test.py
+++
@@ -1,6 +0,0 @@
-# Check that files named "test.py" are checked
-
-
-def test():
-    action = sorted([3, 2, 1])
-    assert action == [1, 2, 3]
--- examples/good/black/noqa/test_cmd.py
+++
@@ -1,6 +0,0 @@
-def test():  # noqa
-    assert 1 + 1 == 2
-
-
-def test_multi_line_args(math_fixture, *args, **kwargs):  # noqa
-    assert 1 + 1 == 2
