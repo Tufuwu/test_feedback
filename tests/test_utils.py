b1b61e76500dd758aa5ae4d5231f784005eead03
wu <wu@.com>
2025-08-24 21:42:00 +0800

    earwig/mwparserfromhell/gpt-4o-3

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,76 +1,61 @@
-#!/usr/bin/env python
+# Copyright (C) 2012-2016 Ben Kurtovic <ben.kurtovic@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-import io
-import os
-import sys
-import tempfile
+"""
+Tests for the utils module, which provides parse_anything().
+"""
 
-from fitparse.utils import fileish_open, is_iterable
+import pytest
 
-if sys.version_info >= (2, 7):
-    import unittest
-else:
-    import unittest2 as unittest
-
-
-def testfile(filename):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'files', filename)
-
-
-class UtilsTestCase(unittest.TestCase):
-
-    def test_fileish_open_read(self):
-        """Test the constructor does the right thing when given different types
-        (specifically, test files with 8 characters, followed by an uppercase.FIT
-        extension), which confused the fileish check on Python 2, see
-        https://github.com/dtcooper/python-fitparse/issues/29#issuecomment-312436350
-        for details"""
-
-        def test_fopen(fileish):
-            with fileish_open(fileish, 'rb') as f:
-                self.assertIsNotNone(f.read(1))
-                f.seek(0, os.SEEK_SET)
-
-        test_fopen(testfile('nametest.FIT'))
-        with open(testfile("nametest.FIT"), 'rb') as f:
-            test_fopen(f)
-        with open(testfile("nametest.FIT"), 'rb') as f:
-            test_fopen(f.read())
-        with open(testfile("nametest.FIT"), 'rb') as f:
-            test_fopen(io.BytesIO(f.read()))
-
-    def test_fileish_open_write(self):
-
-        def test_fopen(fileish):
-            with fileish_open(fileish, 'wb') as f:
-                f.write(b'\x12')
-                f.seek(0, os.SEEK_SET)
-
-        tmpfile = tempfile.NamedTemporaryFile(prefix='fitparse-test', suffix='.FIT', delete=False)
-        filename = tmpfile.name
-        tmpfile.close()
-        try:
-            test_fopen(filename)
-            with open(filename, 'wb') as f:
-                test_fopen(f)
-            test_fopen(io.BytesIO())
-        finally:
-            # remove silently
-            try:
-                os.remove(filename)
-            except OSError:
-                pass
-
-    def test_is_iterable(self):
-        self.assertFalse(is_iterable(None))
-        self.assertFalse(is_iterable(1))
-        self.assertFalse(is_iterable('1'))
-        self.assertFalse(is_iterable(b'1'))
-
-        self.assertTrue(is_iterable((1, 2)))
-        self.assertTrue(is_iterable([1, 2]))
-        self.assertTrue(is_iterable(range(2)))
+from mwparserfromhell.nodes import Template, Text
+from mwparserfromhell.utils import parse_anything
+from .conftest import assert_wikicode_equal, wrap, wraptext
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    "test,valid",
+    [
+        (wraptext("foobar"), wraptext("foobar")),
+        (Template(wraptext("spam")), wrap([Template(wraptext("spam"))])),
+        ("fóóbar", wraptext("fóóbar")),
+        (b"foob\xc3\xa1r", wraptext("foobár")),
+        (123, wraptext("123")),
+        (True, wraptext("True")),
+        (None, wrap([])),
+        ([Text("foo"), Text("bar"), Text("baz")], wraptext("foo", "bar", "baz")),
+        (
+            [wraptext("foo"), Text("bar"), "baz", 123, 456],
+            wraptext("foo", "bar", "baz", "123", "456"),
+        ),
+        ([[[([[((("foo",),),)], "bar"],)]]], wraptext("foo", "bar")),
+    ],
+)
+def test_parse_anything_valid(test, valid):
+    """tests for valid input to utils.parse_anything()"""
+    assert_wikicode_equal(valid, parse_anything(test))
+
+
+@pytest.mark.parametrize(
+    "invalid", [Ellipsis, object, object(), type, ["foo", [object]]]
+)
+def test_parse_anything_invalid(invalid):
+    """tests for invalid input to utils.parse_anything()"""
+    with pytest.raises(ValueError):
+        parse_anything(invalid)
