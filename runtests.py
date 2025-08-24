a09697efac3d8a64ce108085d53586b8670c6bfe
wu <wu@.com>
2025-08-24 23:13:09 +0800

    coleifer/micawber/gpt-4o-4

--- runtests.py
+++ runtests.py
@@ -1,23 +1,88 @@
 #!/usr/bin/env python
-
-"""
-This suite runs tests in django environment. See:
-https://docs.djangoproject.com/en/1.11/topics/testing/advanced/#using-the-django-test-runner-to-test-reusable-applications
-"""
-
 import os
 import sys
+import unittest
 
-import django
-from django.conf import settings
-from django.test.utils import get_runner
+from micawber import tests
 
-if __name__ == "__main__":
-    print('Django: ', django.VERSION)
-    print('Python: ', sys.version)
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
-    django.setup()
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner(interactive=False)
-    failures = test_runner.run_tests(["tests"])
-    sys.exit(bool(failures))
+
+def run_django_tests():
+    try:
+        import django
+    except ImportError:
+        print('Skipping django tests')
+        return
+    else:
+        print('Running django integration tests')
+
+    providers = 'micawber.contrib.mcdjango.mcdjango_tests.tests.test_pr'
+    extensions = (
+        ('oembed_no_urlize', {'urlize_all': False}),
+    )
+
+    from django.conf import settings
+    if not settings.configured:
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    },
+                },
+            SITE_ID=1,
+            INSTALLED_APPS=[
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.sites',
+                'micawber.contrib.mcdjango',
+                'micawber.contrib.mcdjango.mcdjango_tests',
+            ],
+            TEMPLATES=[
+                {
+                    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                    'DIRS': [],
+                    'APP_DIRS': True,
+                    'OPTIONS': {}
+                },
+            ],
+            MICAWBER_PROVIDERS=providers,
+            MICAWBER_TEMPLATE_EXTENSIONS=extensions,
+        )
+    else:
+        settings.MICAWBER_PROVIDERS = providers
+        settings.MICAWBER_TEMPLATE_EXTENSIONS = extensions
+
+    try:
+        from django import setup
+    except ImportError:
+        pass
+    else:
+        setup()
+
+    from django.test.runner import DiscoverRunner
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+    return DiscoverRunner().run_tests(['micawber/contrib/mcdjango'])
+
+
+def runtests(*test_args):
+    print("Running micawber tests")
+    errors = failures = False
+    suite = unittest.TestLoader().loadTestsFromModule(tests)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if result.failures:
+        failures = True
+    if result.errors:
+        errors = True
+    if not (errors or failures):
+        print("All micawber tests passed")
+
+    dj_failures = run_django_tests()
+
+    if failures or errors or dj_failures:
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-requires = []
-with open('requirements.txt') as f:
-    for line in f.readlines():
-        line = line.strip()  # Remove spaces
-        line = line.split('#')[0]  # Remove comments
-        if line:  # Remove empty lines
-            requires.append(line)
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
 
 setup(
-    name='django-clickhouse',
-    version='1.0.3',
-    packages=['django_clickhouse'],
-    package_dir={'': 'src'},
-    url='https://github.com/carrotquest/django-clickhouse',
-    license='BSD 3-clause "New" or "Revised" License',
-    author='Carrot quest',
-    author_email='m1ha@carrotquest.io',
-    description='Django extension to integrate with ClickHouse database',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requires
+    name='micawber',
+    version='0.5.1',
+    description='a small library for extracting rich content from urls',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/micawber/',
+    packages=[p for p in find_packages() if not p.startswith('examples')],
+    package_data = {
+        'micawber': [
+            'contrib/mcdjango/templates/micawber/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    test_suite='runtests.runtests',
 )
