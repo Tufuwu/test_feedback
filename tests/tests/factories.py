742d59677aa9f1973ee4191a03799eba8be3f02e
wu <wu@.com>
2025-08-24 21:38:49 +0800

    django-cms/django-sekizai/gpt-4o-3

--- tests/tests/factories.py
+++
@@ -1,8 +0,0 @@
-import factory
-
-from rest_framework_tus.models import get_upload_model
-
-
-class UploadFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = get_upload_model()
