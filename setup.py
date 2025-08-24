34213914c20a26cfef033aadc7a49221dbe7f33a
wu <wu@.com>
2025-08-24 23:33:10 +0800

    cdent/gabbi/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,48 +1,21 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
 
 
-setup(
-    name="bundlewrap",
-    version="4.3.0",
-    description="Config management with Python",
-    long_description=(
-        "By allowing for easy and low-overhead config management, BundleWrap fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
-        "While most other config management systems rely on a client-server architecture, BundleWrap works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
-    ),
-    author="Torsten Rehn",
-    author_email="torsten@rehn.email",
-    license="GPLv3",
-    url="http://bundlewrap.org",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "bw=bundlewrap.cmdline:main",
-        ],
-    },
-    keywords=["configuration", "config", "management"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Systems Administration",
-    ],
-    install_requires=[
-        "cryptography",
-        "Jinja2",
-        "Mako",
-        "passlib",
-        "pyyaml",
-        "requests >= 1.0.0",
-        "tomlkit",
-    ],
-    zip_safe=False,
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
