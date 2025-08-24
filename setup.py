f130de5b4675245ca53673ff7ad9855d71a5471c
wu <wu@.com>
2025-08-24 23:55:02 +0800

    roverdotcom/django-inlinecss/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,29 +1,137 @@
 import os
-from setuptools import setup, find_packages
+import sys
+from shutil import rmtree
 
-from tos import VERSION
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-readme = f.read()
-f.close()
+# Package meta-data.
+NAME = 'django-inlinecss'
+SRC_DIR = 'django_inlinecss'
+DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
+URL = 'https://github.com/roverdotcom/django-inlinecss'
+EMAIL = 'philip@rover.com'
+AUTHOR = 'Philip Kimmey'
+REQUIRES_PYTHON = '>=3.8'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'Django>=3.2',
+    'pynliner',
+    'future>=0.16.0',
+]
+
+# What packages are required only for tests?
+TESTS = [
+    'mock==5.1.0',
+    'pytest==8.0.1',
+    'pytest-django==4.8.0',
+]
+
+# What packages are optional?
+EXTRAS = {
+    'flake8': [
+        'flake8==7.0.0',
+        'flake8-isort==6.1.1',
+        'isort==5.13.2',
+        'testfixtures==8.0.0',
+    ],
+    "tests": TESTS,
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+
+try:
+    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print(f"\033[1m{s}\033[0m")
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
 
 setup(
-    name='django-tos',
-    version=".".join(map(str, VERSION)),
-    description='django-tos is a reusable Django application for setting Terms of Service.',
-    long_description=readme,
-    author='Frank Wiles',
-    author_email='frank@revsys.com',
-    url='http://github.com/revsys/django-tos/tree/master',
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    license="MIT",
+    url=URL,
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
+    keywords=["html", "css", "inline", "style", "email"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Email",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
+    install_requires=REQUIRED,
+    tests_require=TESTS,
+    extras_require=EXTRAS,
+    # $ setup.py upload support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
