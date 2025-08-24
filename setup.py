fe521adacb349bf21ba1257b5fa06947f552ac97
wu <wu@.com>
2025-08-24 19:07:02 +0800

    exxamalte/python-georss-ingv-centro-nazionale-terremoti-client/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,34 +1,46 @@
-# -*- coding: utf-8 -*-
+import os
 
 from setuptools import find_packages, setup
 
-exec(open('etesync_dav/_version.py').read())
+NAME = "georss_ingv_centro_nazionale_terremoti_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "A GeoRSS client library for the INGV Centro Nazionale Terremoti (Earthquakes) feed."
+URL = (
+    "https://github.com/exxamalte/python-georss-ingv-centro-nazionale-terremoti-client"
+)
+
+REQUIRES = [
+    "georss_client>=0.14",
+]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION = {}
+with open(os.path.join(HERE, NAME, "__version__.py")) as f:
+    exec(f.read(), VERSION)  # pylint: disable=exec-used
 
 setup(
-    name='etesync-dav',
-    version=__version__,
-    author='Tom Hacohen',
-    author_email='tom@stosb.com',
-    url='https://github.com/etesync/etesync-dav',
-    description='A CalDAV and CardDAV frontend for EteSync',
-    keywords=['etesync', 'encryption', 'sync', 'pim', 'caldav', 'carddav'],
-    license='GPL-3.0-only',
-    long_description=open('DESCRIPTION.rst').read(),
-    packages=find_packages(),
-    scripts=[
-        'scripts/etesync-dav',
+    name=NAME,
+    version=VERSION["__version__"],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    license="Apache-2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=find_packages(exclude=("tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    include_package_data=True,
-    python_requires='>=3',
-    install_requires=[
-        'appdirs>=1.4.3',
-        'etesync>=0.12.1',
-        'etebase>=0.30.0',
-        'msgpack>=1.0.0',
-        'Radicale>=3.0.3,<=3.1.0',
-        'Flask>=1.1.1',
-        'Flask-WTF>=0.14.2',
-        'requests[socks]>=2.21',
-        'pyobjc-framework-Cocoa>=7.0.0 ; sys_platform=="darwin"',
-    ]
+    install_requires=REQUIRES,
 )
