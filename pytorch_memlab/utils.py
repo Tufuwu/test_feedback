8523ecb290e6786077427cbb3034a658e6142f62
wu <wu@.com>
2025-08-24 19:31:49 +0800

    tartley/colorama/gpt-4o-2

--- pytorch_memlab/utils.py
+++
@@ -1,5 +0,0 @@
-from math import isnan
-from calmsize import size as calmsize
-
-def readable_size(num_bytes: int) -> str:
-    return '' if isnan(num_bytes) else '{:.2f}'.format(calmsize(num_bytes))
