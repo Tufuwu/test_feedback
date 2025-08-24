6702f9857d4ed182a531b267e6676309bc384e6b
wu <wu@.com>
2025-08-24 18:30:13 +0800

    cuducos/webassets-elm/gpt-4o-2

--- scout/constants.py
+++
@@ -1,7 +0,0 @@
-SEARCH_BM25 = 'bm25'
-SEARCH_SIMPLE = 'simple'
-SEARCH_NONE = 'none'
-RANKING_CHOICES = (SEARCH_BM25, SEARCH_SIMPLE, SEARCH_NONE)
-
-PROTECTED_KEYS = set(('page', 'q', 'key', 'ranking', 'identifier', 'index',
-                      'ordering'))
