aa3eb54bd7fc981ad02de17789b73813f606b81b
wu <wu@.com>
2025-08-24 21:55:29 +0800

    oe-alliance/autobouquetsmaker/gpt-4o-3

--- pydispatch/__init__.py
+++
@@ -1,9 +0,0 @@
-import pkg_resources
-
-try:
-    __version__ = pkg_resources.require('python-dispatch')[0].version
-except: # pragma: no cover
-    __version__ = 'unknown'
-
-from pydispatch.dispatch import Dispatcher
-from pydispatch.properties import *
