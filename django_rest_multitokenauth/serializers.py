e31515c921761d6ebb9e5a00c5b85654a8345215
wu <wu@.com>
2025-08-24 21:30:49 +0800

    anymail/django-anymail/gpt-4o-3

--- django_rest_multitokenauth/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-__all__ = [
-    'EmailSerializer',
-]
-
-
-class EmailSerializer(serializers.Serializer):
-    email = serializers.EmailField()
--- tests/urls.py
+++
@@ -1,6 +0,0 @@
-""" Tests App URL Config """
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r'^api/auth/', include('django_rest_multitokenauth.urls', namespace='multi_token_auth')),
-]
