b128b18c5de475daa63c4c0e0328e52720d6a1b3
wu <wu@.com>
2025-08-25 11:32:12 +0800

    lgoodridge/django-uniauth/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,71 +1,43 @@
-# The MIT License (MIT)
-# Copyright (c) 2017 Karl-Petter Lindegaard
+import setuptools
 
-import re
-import os
-from io import open
-from setuptools import setup
+with open("README.md", "r") as readme:
+    long_description = readme.read()
 
-
-def read_file(fname, encoding='utf-8'):
-    with open(fname, encoding=encoding) as r:
-        return r.read()
-
-
-def find_version(*file_paths):
-    fpath = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = read_file(fpath)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-
-    err_msg = 'Unable to find version string in {}'.format(fpath)
-    raise RuntimeError(err_msg)
-
-
-README = read_file('README.md')
-version = find_version('smbus2', '__init__.py')
-test_deps = [
-    'mock;python_version<"3.3"',
-    'nose'
-]
-
-setup(
-    name="smbus2",
-    version=version,
-    author="Karl-Petter Lindegaard",
-    author_email="kp.lindegaard@gmail.com",
-    description="smbus2 is a drop-in replacement for smbus-cffi/smbus-python in pure Python",
-    license="MIT",
-    keywords=['smbus', 'smbus2', 'python', 'i2c', 'raspberrypi', 'linux'],
-    url="https://github.com/kplindegaard/smbus2",
-    packages=['smbus2'],
-    package_data={'smbus2': ['py.typed', 'smbus2.pyi']},
-    long_description=README,
+setuptools.setup(
+    name="django-uniauth",
+    version="1.4.1",
+    author="Lance Goodridge",
+    author_email="ldgoodridge95@gmail.com",
+    keywords=["django", "auth", "authentication", "cas", "sso", "single sign-on"],
+    description="A Django app for managing CAS and custom user authentication.",
+    include_package_data=True,
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    extras_require={
-        'docs': [
-            'sphinx >= 1.5.3'
-        ],
-        'qa': [
-            'flake8'
-        ],
-        'test': test_deps
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+    url="https://github.com/lgoodridge/django-uniauth",
+    license='LGPLv3',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=[
+        "Django>=1.11",
+        "python-cas>=1.4.0",
+        "djangorestframework-simplejwt>=4.1.0",
     ],
+    extras_require = {
+        ":python_version<='3.2'": ["mock"],
+    },
+    packages=setuptools.find_packages(exclude=["demo-app",]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2",
+        "Framework :: Django :: 3",
+        "Framework :: Django :: 4",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ]
 )
