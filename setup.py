0e14c78d4a77ebefe6df66ded038e109fd589e7b
wu <wu@.com>
2025-08-25 11:24:06 +0800

    dlenski/python-vipaccess/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,57 +1,50 @@
-import os
+#!/usr/bin/env python3
+from setuptools import setup
+from io import open
 
-from setuptools import find_packages, setup
-
-VERSION = __import__("import_export").__version__
-
-CLASSIFIERS = [
-    'Framework :: Django',
-    'Framework :: Django :: 2.2',
-    'Framework :: Django :: 3.1',
-    'Framework :: Django :: 3.2',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3 :: Only',
-    'Topic :: Software Development',
-]
-
-install_requires = [
-    'diff-match-patch',
-    'Django>=2.2',
-    'tablib[html,ods,xls,xlsx,yaml]>=3.0.0',
-]
-
-
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
-    readme = f.read()
-
+with open('description.rst', encoding='utf-8') as f:
+    description = f.read()
 
 setup(
-    name="django-import-export",
-    description="Django application and library for importing and exporting"
-                " data with included admin integration.",
-    long_description=readme,
-    version=VERSION,
-    author="Informatika Mihelac",
-    author_email="bmihelac@mihelac.org",
-    license='BSD License',
-    platforms=['OS Independent'],
-    url="https://github.com/django-import-export/django-import-export",
-    project_urls={
-        "Documentation": "https://django-import-export.readthedocs.io/en/stable/",
-        "Changelog": "https://django-import-export.readthedocs.io/en/stable/changelog.html",
+    name='python-vipaccess',
+    version='0.13',
+    description="A free software implementation of Symantec's VIP Access application and protocol",
+    long_description=description,
+    url='https://github.com/dlenski/python-vipaccess',
+    author='Daniel Lenski',
+    author_email='dlenski@gmail.com',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='development',
+    packages=['vipaccess'],
+    install_requires=[
+        # verify consistency with requirements.txt
+        'pycryptodome>=3.6.6',
+        'oath>=1.4.1',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vipaccess=vipaccess.__main__:main',
+        ],
     },
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    install_requires=install_requires,
-    python_requires=">=3.6",
-    classifiers=CLASSIFIERS,
-    zip_safe=False,
+    test_requires=[
+        'nose>=1.0',
+    ],
+    test_suite='nose.collector',
 )
