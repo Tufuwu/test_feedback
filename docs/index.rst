ca962f3a6238c5b5fd0e4d805ff064e75f6f7aec
wu <wu@.com>
2025-08-25 11:49:17 +0800

    zamzterz/flask-pyoidc/gpt-4o-5

--- docs/index.rst
+++ docs/index.rst
@@ -1,15 +1,19 @@
-.. include:: ../README.rst
+Welcome to flask-pyoidc's documentation!
+========================================
 
-.. include:: internals.rst
+In addition to this documentation, you may have a look on some 
+`example code`_
 
-Show me the code
-----------------
-Check the code to see details.
+.. _example code: https://github.com/zamzterz/Flask-pyoidc/tree/master/example
 
 .. toctree::
    :maxdepth: 2
+   :caption: Contents:
 
+   quickstart
+   configuration
    api
+
 
 Indices and tables
 ==================
--- setup.py
+++ setup.py
@@ -1,55 +1,25 @@
-"""
-Python toolkit to work with BUFR messages.
-"""
-import os
-
 from setuptools import setup
 
-
-def get_version():
-    with open(os.path.join(os.path.dirname(__file__), 'pybufrkit', '__init__.py')) as ins:
-        for line in ins.readlines():
-            if line.startswith('__version__'):
-                return line.split('=')[1].strip()[1:-1]
-
-
-def get_requirements():
-    requirements = ['bitstring>=3.1.3', 'six']
-    return requirements
-
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
-    name='pybufrkit',
-    version=get_version(),
-    platforms=['any'],
-    packages=['pybufrkit'],
-    package_dir={'pybufrkit': 'pybufrkit'},
-    include_package_data=True,
-    setup_requires=["pytest-runner"],
-    install_requires=get_requirements(),
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': ['pybufrkit = pybufrkit:main'],
-    },
-
-    author='Yang Wang',
-    author_email='ywangd@gmail.com',
-    description='Python toolkit to work with BUFR files',
-    long_description=__doc__,
-    license='MIT',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Utilities",
+    name='Flask-pyoidc',
+    version='3.9.0',
+    packages=['flask_pyoidc'],
+    package_dir={'': 'src'},
+    url='https://github.com/zamzterz/flask-pyoidc',
+    license='Apache 2.0',
+    author='Samuel Gulliksson',
+    author_email='samuel.gulliksson@gmail.com',
+    description='Flask extension for OpenID Connect authentication.',
+    install_requires=[
+        'oic>=1.2.1',
+        'Flask',
+        'requests',
+        'importlib_resources'
     ],
-    keywords=['BUFR', 'WMO'],
-    url='https://github.com/ywangd/pybufrkit',
+    package_data={'flask_pyoidc': ['parse_fragment.html']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
