d4122046861d2f7e16c81213be3a29c3e5f7fb31
wu <wu@.com>
2025-08-25 11:15:31 +0800

    tompollard/tableone/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,57 +1,93 @@
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-import os
+from pathlib import Path
 
-with open('tenable/version.py', 'r') as vfile:
-    exec(vfile.read())
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-try:
-    long_description = open(
-        os.path.join(
-            os.path.abspath(os.path.dirname(__file__)),
-            'README.rst')).read()
-except:
-    long_description = 'Please refer to https://pytenable.readthedocs.io'
-    print('! could not read README.rst file.')
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='pyTenable',
-    version=version,
-    description='Python library to interface into Tenable\'s products and applications',
-    author='Tenable, Inc.',
+    name='tableone',
+
+    # Versions should comply with PEP440. For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.7.11',
+
+    description='TableOne',
     long_description=long_description,
-    author_email='smcgrath@tenable.com',
-    url='https://github.com/tenable/pytenable',
+    long_description_content_type='text/markdown',
+
+    # The project's main homepage.
+    url='https://github.com/tompollard/tableone',
+
+    # Author details
+    author='Tom Pollard',
+    author_email='tpollard@mit.edu',
+
+    # Choose your license
     license='MIT',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    keywords='tenable tenable_io securitycenter containersecurity',
-    packages=find_packages(exclude=['docs', 'tests']),
+
+    # What does your project relate to?
+    keywords='Table one Table 1 clinical research population cohort',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['tableone'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    # py_modules=['tableone', 'modality'],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'requests>=2.19',
-        'python-dateutil>=2.6',
-        'semver>=2.8.1',
-        'ipaddress>=1.0.22',
-        'restfly>=1.3.5',
-        'marshmallow>=3.6',
-        'python-box>=4.0',
-    ],
-    extras_require={
-        'NessusReportv2': ['defusedxml>=0.5.0'],
-        'PWCertAuth': ['requests-pkcs12>=1.3'],
-        'docker': ['docker>=3.7.2'],
-        'complete': [
-            'defusedxml>=0.5.0',
-            'requests-pkcs12>=1.3',
-            'docker>=3.7.2',
-        ]
-    }
+        'numpy>=1.23.2',
+        'pandas>=1.4.3',
+        'scipy>=1.9.0',
+        'statsmodels>=0.13.2',
+        'tabulate>=0.8.10'
+        ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={'wfdb': ['wfdb.config'],
+    # },
+    include_package_data=True,
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('config', ['wfdb.config'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+
 )
