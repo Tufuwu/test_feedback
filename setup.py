4791c4b784690763a7c8c201d44aaa1ae09bfab8
wu <wu@.com>
2025-08-25 11:28:50 +0800

    hbehrens/puncover/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,68 +1,72 @@
-#!/usr/bin/env python3
-#
-# Copyright (c) 2016 Grigori Goronzy <greg@chown.ath.cx>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+#!/usr/bin/env python
+import os
+from pathlib import Path
 
-import stcgal
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
-with open("doc/PyPI.md", "r") as fh:
-    long_description = fh.read()
+ROOTDIR = Path(__file__).parent
+
+__version__ = None  # Overwritten by executing version.py.
+with open(ROOTDIR / "puncover/version.py") as f:
+    exec(f.read())
+
+
+with open(ROOTDIR / "requirements-test.txt") as f:
+    tests_require = list(filter(lambda x: not x.strip().startswith('-r'), f.readlines()))
+
+with open(ROOTDIR / "requirements.txt") as f:
+    requires = f.readlines()
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+
+    # http://stackoverflow.com/a/3780822/196350
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
+
 
 setup(
-    name = "stcgal",
-    version = stcgal.__version__,
-    packages = find_packages(exclude=["doc", "tests"]),
-    install_requires = ["pyserial>=3.0", "tqdm>=4.0.0"],
-    extras_require = {
-        "usb": ["pyusb>=1.0.0"]
-    },
-    entry_points = {
-        "console_scripts": [
-            "stcgal = stcgal.frontend:cli",
-        ],
-    },
-    description = "STC MCU ISP flash tool",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    keywords = "stc mcu microcontroller 8051 mcs-51",
-    url = "https://github.com/grigorig/stcgal",
-    author = "Grigori Goronzy",
-    author_email = "greg@kinoho.net",
-    license = "MIT License",
-    platforms = "any",
-    classifiers = [
+    name="puncover",
+    version=__version__,
+    description="Analyses C/C++ build output for code size, static variables, and stack usage.",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    url="https://github.com/hbehrens/puncover",
+    download_url="https://github.com/hbehrens/puncover/tarball/%s" % __version__,
+    author="Heiko Behrens",
+    license="MIT",
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
         "Intended Audience :: Developers",
+        "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development :: Embedded Systems",
-        "Topic :: Software Development",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    test_suite = "tests",
-    tests_require = ["PyYAML"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"console_scripts": ["puncover = puncover.puncover:main"]},
+    install_requires=requires,
+    tests_require=tests_require,
+    cmdclass={
+        "clean": CleanCommand,
+    },
+    # TODO: https://github.com/HBehrens/puncover/issues/36
+    #  Fix Python 3.5
+    python_requires=">=3.6",
 )
