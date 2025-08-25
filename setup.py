c6510844db813b60defc88b924779be911935f78
wu <wu@.com>
2025-08-25 11:31:52 +0800

    kplindegaard/smbus2/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,53 +1,71 @@
-from setuptools import setup, find_packages
+# The MIT License (MIT)
+# Copyright (c) 2017 Karl-Petter Lindegaard
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst', 'md')
-except ImportError:
-    print("Warning: pypandoc module not found, could not convert Markdown to RST")
-    long_description = open('README.md', 'r').read()
-
-
-def _is_requirement(line):
-    """Returns whether the line is a valid package requirement."""
-    line = line.strip()
-    return line and not (line.startswith("-r") or line.startswith("#"))
+import re
+import os
+from io import open
+from setuptools import setup
 
 
-def _read_requirements(filename):
-    """Returns a list of package requirements read from the file."""
-    requirements_file = open(filename).read()
-    return [line.strip() for line in requirements_file.splitlines()
-            if _is_requirement(line)]
+def read_file(fname, encoding='utf-8'):
+    with open(fname, encoding=encoding) as r:
+        return r.read()
 
 
-required_packages = _read_requirements("requirements/base.txt")
-test_packages = _read_requirements("requirements/tests.txt")
+def find_version(*file_paths):
+    fpath = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = read_file(fpath)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+
+    err_msg = 'Unable to find version string in {}'.format(fpath)
+    raise RuntimeError(err_msg)
+
+
+README = read_file('README.md')
+version = find_version('smbus2', '__init__.py')
+test_deps = [
+    'mock;python_version<"3.3"',
+    'nose'
+]
 
 setup(
-    name='rapidpro-python',
-    version=__import__('temba_client').__version__,
-    description='Python client library for the RapidPro',
-    long_description=long_description,
-
-    keywords='rapidpro client',
-    url='https://github.com/rapidpro',
-    license='BSD',
-
-    author='Nyaruka',
-    author_email='code@nyaruka.com',
-
+    name="smbus2",
+    version=version,
+    author="Karl-Petter Lindegaard",
+    author_email="kp.lindegaard@gmail.com",
+    description="smbus2 is a drop-in replacement for smbus-cffi/smbus-python in pure Python",
+    license="MIT",
+    keywords=['smbus', 'smbus2', 'python', 'i2c', 'raspberrypi', 'linux'],
+    url="https://github.com/kplindegaard/smbus2",
+    packages=['smbus2'],
+    package_data={'smbus2': ['py.typed', 'smbus2.pyi']},
+    long_description=README,
+    long_description_content_type="text/markdown",
+    extras_require={
+        'docs': [
+            'sphinx >= 1.5.3'
+        ],
+        'qa': [
+            'flake8'
+        ],
+        'test': test_deps
+    },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
     ],
-
-    packages=find_packages(),
-    install_requires=required_packages,
-
-    test_suite='nose.collector',
-    tests_require=required_packages + test_packages,
 )
