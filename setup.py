e204839c3c60ed4c5eaa1c115d26ff2cfaacfa28
wu <wu@.com>
2025-08-25 11:33:08 +0800

    mahjongrepository/mahjong/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
-#!/usr/bin/env python3
+import setuptools
 
-from setuptools import setup
-from setuptools import find_packages
+from distutils.core import setup
 
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('CHANGELOG.rst', 'r') as f:
+    changelog = f.read()
 
 setup(
-    name="litespi",
-    description="Small footprint and configurable SPI core",
-    author="LiteSPI Developers",
-    url="https://github.com/litex-hub",
-    download_url="https://github.com/litex-hub/litespi",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
+    name='mahjong',
+    packages=[
+        'mahjong',
+        'mahjong.hand_calculating',
+        'mahjong.hand_calculating.yaku_list',
+        'mahjong.hand_calculating.yaku_list.yakuman',
+    ],
+    version='1.1.9',
+    description='Mahjong hands calculation',
+    long_description=readme + '\n\n' + changelog,
+    author='Alexey Lisikhin',
+    author_email='lisikhin@gmail.com',
+    url='https://github.com/MahjongRepository/mahjong',
+    data_files=[('', ['README.rst', 'CHANGELOG.rst'])],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
