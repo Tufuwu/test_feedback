e3e66361bddbf145596b8bdc4ad86f7452ded879
wu <wu@.com>
2025-08-25 11:39:48 +0800

    python-trio/trustme/gpt-4o-5

--- README.rst
+++ README.rst
@@ -1,27 +1,107 @@
-##########################
-The Python Benchmark Suite
-##########################
+.. note that this README gets 'include'ed into the main documentation
 
-.. image:: https://img.shields.io/pypi/v/pyperformance.svg
-   :alt: Latest pyperformance release on the Python Cheeseshop (PyPI)
-   :target: https://pypi.python.org/pypi/pyperformance
+==============================================
+ trustme: #1 quality TLS certs while you wait
+==============================================
 
-.. image:: https://travis-ci.com/python/pyperformance.svg?branch=master
-   :alt: Build status of pyperformance on Travis CI
-   :target: https://travis-ci.com/github/python/pyperformance
+.. image:: https://vignette2.wikia.nocookie.net/jadensadventures/images/1/1e/Kaa%27s_hypnotic_eyes.jpg/revision/latest?cb=20140310173415
+   :width: 200px
+   :align: right
 
-The ``pyperformance`` project is intended to be an authoritative source of
-benchmarks for all Python implementations. The focus is on real-world
-benchmarks, rather than synthetic benchmarks, using whole applications when
-possible.
+You wrote a cool network client or server. It encrypts connections
+using `TLS
+<https://en.wikipedia.org/wiki/Transport_Layer_Security>`__. Your test
+suite needs to make TLS connections to itself.
 
-* `pyperformance documentation <http://pyperformance.readthedocs.io/>`_
-* `pyperformance GitHub project <https://github.com/python/pyperformance>`_
-  (source code, issues)
-* `Download pyperformance on PyPI <https://pypi.python.org/pypi/pyperformance>`_
+Uh oh. Your test suite *probably* doesn't have a valid TLS
+certificate. Now what?
 
-pyperformance is not tuned for PyPy yet: use the `PyPy benchmarks project
-<https://foss.heptapod.net/pypy/benchmarks>`_ instead to measure PyPy
-performances.
+``trustme`` is a tiny Python package that does one thing: it gives you
+a `fake <https://martinfowler.com/bliki/TestDouble.html>`__
+certificate authority (CA) that you can use to generate fake TLS certs
+to use in your tests. Well, technically they're real certs, they're
+just signed by your CA, which nobody trusts. But you can trust
+it. Trust me.
 
-pyperformance is distributed under the MIT license.
+
+Vital statistics
+================
+
+**Install:** ``pip install -U trustme``
+
+**Documentation:** https://trustme.readthedocs.io
+
+**Bug tracker and source code:** https://github.com/python-trio/trustme
+
+**Tested on:** Python 2.7 and Python 3.5+, CPython and PyPy
+
+**License:** MIT or Apache 2, your choice.
+
+**Code of conduct:** Contributors are requested to follow our `code of
+conduct
+<https://github.com/python-trio/trustme/blob/master/CODE_OF_CONDUCT.md>`__
+in all project spaces.
+
+
+Cheat sheet
+===========
+
+.. code-block:: python
+
+   import trustme
+
+   # ----- Creating certs -----
+
+   # Look, you just created your own certificate authority!
+   ca = trustme.CA()
+
+   # And now you issued a cert signed by this fake CA
+   # https://en.wikipedia.org/wiki/Example.org
+   server_cert = ca.issue_cert(u"test-host.example.org")
+
+   # That's it!
+
+   # ----- Using your shiny new certs -----
+
+   # You can configure SSL context objects to trust this CA:
+   ca.configure_trust(ssl_context)
+   # Or configure them to present the server certificate
+   server_cert.configure_cert(ssl_context)
+   # You can use standard library or PyOpenSSL context objects here,
+   # trustme is happy either way.
+
+   # ----- or -----
+                
+   # Save the PEM-encoded data to a file to use in non-Python test
+   # suites:
+   ca.cert_pem.write_to_path("ca.pem")
+   server_cert.private_key_and_cert_chain_pem.write_to_path("server.pem")
+   
+   # ----- or -----
+                
+   # Put the PEM-encoded data in a temporary file, for libraries that
+   # insist on that:
+   with ca.cert_pem.tempfile() as ca_temp_path:
+       requests.get("https://...", verify=ca_temp_path)
+
+
+FAQ
+===
+
+**Should I use these certs for anything real?** Certainly not.
+
+**Why not just use self-signed certificates?** These are more
+realistic. You don't have to disable your certificate validation code
+in your test suite, which is good, because you want to test what you
+run in production, and you would *never* disable your certificate
+validation code in production, right? Plus they're just as easy to
+work with. Actually easier, in many cases.
+
+**What if I want to test how my code handles some really weird TLS
+configuration?** Sure, I'm happy to extend the API to give more
+control over the generated certificates, at least as long as it
+doesn't turn into a second-rate re-export of everything in
+`cryptography <https://cryptography.io>`__. (If you really need a
+fully general X.509 library then they do a great job at that.) `Let's
+talk <https://github.com/python-trio/trustme/issues/new>`__, or send a
+PR.
--- setup.py
+++ setup.py
@@ -1,116 +1,41 @@
-#!/usr/bin/env python3
+from setuptools import setup, find_packages
 
-# FIXME:
-#
-# - REENABLE HG_STARTUP BENCHMARK.
-#
-# Update dependencies:
-#
-#  - python3 -m pip install --user --upgrade pip-tools
-#  - git clean -fdx  # remove all untracked files!
-#  - (cd pyperformance; pip-compile --upgrade requirements.in)
-#
-# Prepare a release:
-#
-#  - git pull --rebase
-#  - Remove untracked files/dirs: git clean -fdx
-#  - maybe update version in pyperformance/__init__.py and doc/conf.py
-#  - set release date in doc/changelog.rst
-#  - git commit -a -m "prepare release x.y"
-#  - run tests: tox --parallel auto
-#  - git push
-#  - check Travis CI status:
-#    https://travis-ci.com/github/python/pyperformance
-#  - check AppVeyor status:
-#    https://ci.appveyor.com/project/lazka/pyperformance-rdqv8
-#
-# Release a new version:
-#
-#  - git tag VERSION
-#  - git push --tags
-#  - Remove untracked files/dirs: git clean -fdx
-#  - python3 setup.py sdist bdist_wheel
-#  - twine upload dist/*
-#
-# After the release:
-#
-#  - set version to n+1: pyperformance/__init__.py and doc/conf.py
-#  - git commit -a -m "post-release"
-#  - git push
+# defines __version__
+exec(open("trustme/_version.py").read())
 
-# Import just to get the version
-import pyperformance
-
-VERSION = pyperformance.__version__
-
-DESCRIPTION = 'Python benchmark suite'
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python',
-]
-
-
-# put most of the code inside main() to be able to import setup.py in
-# unit tests
-def main():
-    import io
-    import os.path
-    from setuptools import setup
-
-    with io.open('README.rst', encoding="utf8") as fp:
-        long_description = fp.read().strip()
-
-    packages = [
-        'pyperformance',
-        'pyperformance.benchmarks',
-        'pyperformance.benchmarks.data',
-        'pyperformance.benchmarks.data.2to3',
-        'pyperformance.tests',
-        'pyperformance.tests.data',
-    ]
-
-    data = {
-        'pyperformance': ['requirements.txt'],
-        'pyperformance.tests': ['data/*.json'],
-    }
-
-    # Search for all files in pyperformance/benchmarks/data/
-    data_dir = os.path.join('pyperformance', 'benchmarks', 'data')
-    benchmarks_data = []
-    for root, dirnames, filenames in os.walk(data_dir):
-        # Strip pyperformance/benchmarks/ prefix
-        root = os.path.normpath(root)
-        root = root.split(os.path.sep)
-        root = os.path.sep.join(root[2:])
-
-        for filename in filenames:
-            filename = os.path.join(root, filename)
-            benchmarks_data.append(filename)
-    data['pyperformance.benchmarks'] = benchmarks_data
-
-    options = {
-        'name': 'pyperformance',
-        'version': VERSION,
-        'author': 'Collin Winter and Jeffrey Yasskin',
-        'license': 'MIT license',
-        'description': DESCRIPTION,
-        'long_description': long_description,
-        'url': 'https://github.com/python/benchmarks',
-        'classifiers': CLASSIFIERS,
-        'packages': packages,
-        'package_data': data,
-        'entry_points': {
-            'console_scripts': ['pyperformance=pyperformance.cli:main']
-        },
-        'install_requires': ["pyperf"],
-    }
-    setup(**options)
-
-
-if __name__ == '__main__':
-    main()
+setup(
+    name="trustme",
+    version=__version__,
+    description=
+      "#1 quality TLS certs while you wait, for the discerning tester",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT -or- Apache License 2.0",
+    packages=find_packages(),
+    url="https://github.com/python-trio/trustme",
+    install_requires=[
+        "cryptography",
+        # cryptography depends on both of these too, so we should declare our
+        # dependencies to be accurate, but they don't actually cost anything:
+        "idna",
+        "ipaddress; python_version < '3.3'",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Networking",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Testing",
+    ])
