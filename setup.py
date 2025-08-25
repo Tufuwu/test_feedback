12eacb385a5de47e94f0a2cd30a1954adf92621c
wu <wu@.com>
2025-08-25 11:42:16 +0800

    scrapy/scrapyd/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,79 +1,60 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
-# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+import sys
 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301 USA
+try:
+    from setuptools import setup
+    using_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    using_setuptools = False
 
-import re
-from io import open
+from os.path import join, dirname
 
-from setuptools import find_packages, setup
+with open(join(dirname(__file__), 'scrapyd/VERSION')) as f:
+    version = f.read().strip()
 
-PACKAGE_NAME = "num2words"
-
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU Library or Lesser General Public License '
-    '(LGPL)',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Topic :: Software Development :: Internationalization',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Software Development :: Localization',
-    'Topic :: Text Processing :: Linguistic',
-]
-
-LONG_DESC = open('README.rst', 'rt', encoding="utf-8").read() + '\n\n' + \
-            open('CHANGES.rst', 'rt', encoding="utf-8").read()
-
-
-def find_version(fname):
-    """Parse file & return version number matching 0.0.1 regex
-    Returns str or raises RuntimeError
-    """
-    version = ''
-    with open(fname, 'r', encoding="utf-8") as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
+setup_args = {
+    'name': 'scrapyd',
+    'version': version,
+    'url': 'https://github.com/scrapy/scrapyd',
+    'description': 'A service for running Scrapy spiders, with an HTTP API',
+    'long_description': open('README.rst').read(),
+    'author': 'Scrapy developers',
+    'maintainer': 'Scrapy developers',
+    'maintainer_email': 'info@scrapy.org',
+    'license': 'BSD',
+    'packages': ['scrapyd'],
+    'include_package_data': True,
+    'zip_safe': False,
+    'classifiers': [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+}
 
 
-setup(
-    name=PACKAGE_NAME,
-    version=find_version("bin/num2words"),
-    description='Modules to convert numbers to words. Easily extensible.',
-    long_description=LONG_DESC,
-    license='LGPL',
-    author='Taro Ogawa <tso at users sourceforge net>',
-    author_email='tos@users.sourceforge.net',
-    maintainer='Savoir-faire Linux inc.',
-    maintainer_email='istvan.szalai@savoirfairelinux.com',
-    keywords=' number word numbers words convert conversion i18n '
-             'localisation localization internationalisation '
-             'internationalization',
-    url='https://github.com/savoirfairelinux/num2words',
-    packages=find_packages(exclude=['tests']),
-    test_suite='tests',
-    classifiers=CLASSIFIERS,
-    scripts=['bin/num2words'],
-    install_requires=["docopt>=0.6.2"],
-    tests_require=['delegator.py'],
-)
+if using_setuptools:
+    setup_args['install_requires'] = [
+        'Twisted>=8.0',
+        'Scrapy>=1.0',
+        'six',
+        'enum-compat',
+    ]
+    setup_args['entry_points'] = {'console_scripts': [
+        'scrapyd = scrapyd.scripts.scrapyd_run:main'
+    ]}
+else:
+    setup_args['scripts'] = ['scrapyd/scripts/scrapyd_run.py']
+
+setup(**setup_args)
